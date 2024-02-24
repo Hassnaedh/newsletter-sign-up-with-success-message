5644e690c09a7e31237608a9489c9acea2e62db2
Hassnaedh <dehbihassnae@gmail.com>
2024-02-24 22:11:58 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
         "grey": "hsl(231, 7%, 60%)",
         "white": "hsl(0, 0%, 100%)",
       },
+      fonFamily: {
+        "roboto": "roboto"
+      }
     },
   },
   plugins: [],
